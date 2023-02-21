1cfad9b21e6f2533e78e641fd35a15a869593aa2
enanesh <edna.ganesha.gr@gmail.com>
2023-02-20 23:38:21 -0800

    Adds service worker

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
@@ -25,6 +26,10 @@ module.exports = () => {
         template: './index.html',
         title: 'Webpack Plugin',
       }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      })
      
     ],
 
